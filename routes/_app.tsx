2349a44863c7f8ec73809240397bea0a33d22c80
Dipankar Maikap <dipankarmaikap77@gmail.com>
2025-12-01 22:40:18 +0530

    feat: base starting point added

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,4 @@
-import { define } from "../../utils.ts";
+import { define } from "../utils.ts";
 
 export default define.page(function App({ Component }) {
   return (
@@ -7,6 +7,16 @@ export default define.page(function App({ Component }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>fresh-project</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <Component />
--- routes/index.tsx
+++ routes/index.tsx
@@ -9,25 +9,11 @@ export default define.page(function Home(ctx) {
   console.log("Shared value " + ctx.state.shared);
 
   return (
-    <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
+    <div class="p-4">
       <Head>
-        <title>Fresh counter</title>
+        <title>Fresh - Deno demo project</title>
       </Head>
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
+      <Counter count={count} />
     </div>
   );
 });
