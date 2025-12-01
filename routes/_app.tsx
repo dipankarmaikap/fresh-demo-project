41cfa55e67e887e5765350b2ac34a6939dd27610
Dipankar Maikap <dipankarmaikap77@gmail.com>
2025-12-01 22:30:24 +0530

    chore: updated packages to latest

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,4 @@
-import { define } from "~/utils.ts";
+import { define } from "../../utils.ts";
 
 export default define.page(function App({ Component }) {
   return (
@@ -6,17 +6,7 @@ export default define.page(function App({ Component }) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>fresh-demo-project</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap"
-          rel="stylesheet"
-        />
+        <title>fresh-project</title>
       </head>
       <body>
         <Component />
